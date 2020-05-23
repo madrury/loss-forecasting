f54f37907d70e1025ade91ec1a9f09664c8e2f3b
Matthew Drury <matthew.drury.83@gmail.com>
2020-05-23 16:26:49 -0700

    Silence d/db log(0) warning when t=0.

--- weibull.py
+++ weibull.py
@@ -8,5 +8,9 @@ def d_alpha_weibull(t: np.array, ul: float, alpha: float, beta: float) -> np.arr
     return - (beta / alpha) * np.exp(-(t / alpha)**beta) * ((t / alpha)**beta)
 
 def d_beta_weibull(t: np.array, ul: float, alpha: float, beta: float) -> np.array:
-    d_beta = np.exp(-(t / alpha)**beta) * ((t / alpha)**beta) * np.log(t / alpha)
-    return np.nan_to_num(d_beta)
+    # When t=0 this will be a logarithm of zero, which will throw a divide by
+    # zero warning when computing the logarithm. The pardial d/dbeta is zero at
+    # t=0, so we silence the warning then infer the zero.
+    with np.errstate(divide='ignore'):
+        log_factor = np.nan_to_num(np.log(t / alpha))
+    return np.exp(-(t / alpha)**beta) * ((t / alpha)**beta) * log_factor
