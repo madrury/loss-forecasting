2b9616e607abb81f8ecca115e456edd9832992e5
Matthew Drury <matthew.drury.83@gmail.com>
2020-05-23 16:47:22 -0700

    Add second derivatives of weibull parameters.

--- weibull.py
+++ weibull.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 def weibull(t: np.array, alpha: float, beta: float) -> np.array:
-    return 1 - np.exp(-(t/alpha)**beta)
+    return 1 - np.exp(-(t / alpha)**beta)
 
 def d_alpha_weibull(t: np.array, alpha: float, beta: float) -> np.array:
-    return - (beta / alpha) * np.exp(-(t / alpha)**beta) * ((t / alpha)**beta)
+    ratio_power = (t / alpha)**beta
+    return - (beta / alpha) * np.exp(- ratio_power) * ratio_power
 
 def d_beta_weibull(t: np.array, alpha: float, beta: float) -> np.array:
     # When t=0 this will be a logarithm of zero, which will throw a divide by
@@ -13,13 +14,21 @@ def d_beta_weibull(t: np.array, alpha: float, beta: float) -> np.array:
     # t=0, so we silence the warning then infer the zero.
     with np.errstate(divide='ignore'):
         log_factor = np.nan_to_num(np.log(t / alpha))
-    return np.exp(-(t / alpha)**beta) * ((t / alpha)**beta) * log_factor
+    ratio_power = (t / alpha)**beta
+    return np.exp(- ratio_power) * ratio_power * log_factor
 
 def d2_alpha_weibull(t: np.array, alpha: float, beta: float):
-    pass
+    ratio_power = (t / alpha)**beta
+    return - (beta / alpha**2) * ratio_power * np.exp(- ratio_power) * (beta * ratio_power - beta - 1)
 
 def d2_beta_weibull(t: np.array, alpha: float, beta: float):
-    pass
+    with np.errstate(divide='ignore'):
+        log_factor = np.nan_to_num(np.log(t / alpha))
+    ratio_power = (t / alpha)**beta
+    return - ratio_power * np.exp(- ratio_power) * log_factor**2 * (ratio_power - 1)
 
 def d_alpha_d_beta_weibull(t: np.array, alpha: float, beta: float):
-    pass
+    with np.errstate(divide='ignore'):
+        log_factor = np.nan_to_num(np.log(t / alpha))
+    ratio_power = (t / alpha)**beta
+    return (1 / alpha) * ratio_power * np.exp(- ratio_power) * (beta * ratio_power * log_factor - beta * log_factor - 1)
