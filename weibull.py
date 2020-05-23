Fix weibull partial derivatives.

weibull.py
@@ -5,9 +5,8 @@ def weibull(t: np.array, ul: float, alpha: float, beta: float) -> np.array:
     return ul * (1 - np.exp(-(t/alpha)**beta))
 
 def d_alpha_weibull(t: np.array, ul: float, alpha: float, beta: float) -> np.array:
-    w = weibull(t, ul, alpha, beta)
-    return (beta / alpha) * (1 + w) * ((t / alpha)**beta)
+    return - (beta / alpha) * np.exp(-(t / alpha)**beta) * ((t / alpha)**beta)
 
 def d_beta_weibull(t: np.array, ul: float, alpha: float, beta: float) -> np.array:
-    w = weibull(t, ul, alpha, beta)
-    return - (1 + w) * ((t / alpha)**beta) * np.log(t / alpha)
+    d_beta = np.exp(-(t / alpha)**beta) * ((t / alpha)**beta) * np.log(t / alpha)
+    return np.nan_to_num(d_beta)