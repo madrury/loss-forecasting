f8bf66a2c2abd47a75b56ce620df28e2cd684dac
Matthew Drury <matthew.drury.83@gmail.com>
2020-06-12 10:06:04 -0700

    Add utils.py

---
+++ loss_development/utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def diff_within_groups(x, groups):
+    group_boundaries = np.diff(groups).astype(bool)
+    return np.diff(x)[~group_boundaries]
+
+def count_elements_in_groups(groups):
+    return np.diff(np.concatenate([
+        [0],
+        # Look for indexes of boundaries between groups.
+        np.where(np.diff(groups) == 1)[0] + 1,
+        [len(groups)]
+    ]))
